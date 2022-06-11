3b140823b8c37b2d9c9eb90142ecb1b55fe160a9
0xph03n1x <lazarovmihail97@gmail.com>
2022-06-11 23:26:33 +0300

    cargo test

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\User\Desktop\rust\rust_testing\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\User\Desktop\rust\rust_testing\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\User\Desktop\rust\rust_testing\hello_cargo\target\debug\hello_cargo.exe: C:\Users\User\Desktop\rust\rust_testing\hello_cargo\src\main.rs
